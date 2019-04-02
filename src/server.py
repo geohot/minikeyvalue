c392cecf8c8a79b3e61ffd897bc5179ebf20bc50
George Hotz <geohot@gmail.com>
2019-04-02 00:56:48 -0700

    add xattr, fix up start scripts

--- src/server.py
+++ src/server.py
@@ -1,11 +1,17 @@
 import os
 import time
 import json
+import xattr
 import random
 import socket
 import hashlib
+import tempfile
 
 print("hello", os.environ['TYPE'], os.getpid())
+
+def resp(start_response, code, headers=[('Content-type', 'text/plain')], body=b''):
+  start_response(code, headers)
+  return [body]
 
 # *** Master Server ***
 
@@ -19,50 +25,53 @@ if os.environ['TYPE'] == "master":
   import plyvel
   db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
-def master(env, start_response):
+def master(env, sr):
   key = env['REQUEST_URI']
-  metakey = db.get(key.encode('utf-8'))
 
+  if env['REQUEST_METHOD'] == 'POST':
+    # POST is called by the volume servers to write to the database
+    flen = int(env.get('CONTENT_LENGTH', '0'))
+    if flen > 0:
+      db.put(key.encode('utf-8'), env['wsgi.input'].read(), sync=True)
+    else:
+      db.delete(key.encode('utf-8'))
+    return resp(sr, '200 OK')
+
+  metakey = db.get(key.encode('utf-8'))
   if metakey is None:
     if env['REQUEST_METHOD'] == 'PUT':
       # handle putting key
       # TODO: make volume selection intelligent
       volume = random.choice(volumes)
-
-      # save volume to database
-      meta = {"volume": volume}
-      db.put(key.encode('utf-8'), json.dumps(meta).encode('utf-8'))
     else:
       # this key doesn't exist and we aren't trying to create it
-      start_response('404 Not Found', [('Content-type', 'text/plain')])
-      return [b'key not found']
+      return resp(sr, '404 Not Found')
   else:
-    # key found and we are trying to put it
-    """
+    # key found 
     if env['REQUEST_METHOD'] == 'PUT':
-      start_response('409 Conflict', [('Content-type', 'text/plain')])
-      return [b'key already exists']
-    """
+      # we are trying to put it. delete first!
+      return resp(sr, '409 Conflict')
     meta = json.loads(metakey.decode('utf-8'))
+    volume = meta['volume']
 
   # send the redirect
-  print(meta)
-  volume = meta['volume']
   headers = [('Location', 'http://%s%s' % (volume, key))]
-  start_response('307 Temporary Redirect', headers)
-  return [b""]
+
+  return resp(sr, '307 Temporary Redirect', headers)
 
 # *** Volume Server ***
 
 class FileCache(object):
+  # this is a single computer on disk key value store
+
   def __init__(self, basedir):
     self.basedir = os.path.realpath(basedir)
-    os.makedirs(self.basedir, exist_ok=True)
+    self.tmpdir = os.path.join(self.basedir, "tmp")
+    os.makedirs(self.tmpdir, exist_ok=True)
     print("FileCache in %s" % basedir)
 
   def k2p(self, key, mkdir_ok=False):
-    # must be MD5 hash
-    assert len(key) == 32
+    key = hashlib.md5(key).hexdigest()
 
     # 2 layers deep in nginx world
     path = self.basedir+"/"+key[0:2]+"/"+key[0:4]
@@ -79,42 +88,52 @@ class FileCache(object):
     os.unlink(self.k2p(key))
 
   def get(self, key):
-    return open(self.k2p(key), "rb").read()
+    return open(self.k2p(key), "rb")
 
-  def put(self, key, value):
-    # TODO: refactor to use a tempfile and symlink
-    with open(self.k2p(key, True), "wb") as f:
-      f.write(value)
+  def put(self, key, stream):
+    with tempfile.NamedTemporaryFile(dir=self.tmpdir, delete=False) as f:
+      # TODO: in chunks, don't waste RAM
+      f.write(stream.read())
+
+      # save the real name in xattr in case we rebuild it
+      xattr.setxattr(f.name, 'user.key', key)
+
+      # TODO: check hash
+      os.rename(f.name, self.k2p(key, True))
 
 if os.environ['TYPE'] == "volume":
-  host = socket.gethostname()
+  host = os.environ['HOST'] + ":" + os.environ['PORT']
+  print(host)
 
   # create the filecache
   fc = FileCache(os.environ['VOLUME'])
 
-def volume(env, start_response):
+def volume(env, sr):
   key = env['REQUEST_URI'].encode('utf-8')
-  hkey = hashlib.md5(key).hexdigest()
-  print(hkey)
-
-  if env['REQUEST_METHOD'] == 'GET':
-    if not fc.exists(hkey):
-      # key not in the FileCache
-      start_response('404 Not Found', [('Content-type', 'text/plain')])
-      return [b'key not found']
-    start_response('200 OK', [('Content-type', 'text/plain')])
-    return [fc.get(hkey)]
 
   if env['REQUEST_METHOD'] == 'PUT':
+    if fc.exists(key):
+      # can't write, already exists
+      return resp(sr, '409 Conflict')
+
     flen = int(env.get('CONTENT_LENGTH', '0'))
     if flen > 0:
-      fc.put(hkey, env['wsgi.input'].read(flen))
-      start_response('200 OK', [('Content-type', 'text/plain')])
-      return [b'']
+      fc.put(key, env['wsgi.input'])
+      # notify database
+
+      return resp(sr, '201 Created')
     else:
-      start_response('411 Length Required', [('Content-type', 'text/plain')])
-      return [b'']
+      return resp(sr, '411 Length Required')
+
+  if not fc.exists(key):
+    # key not in the FileCache, 404
+    return resp(sr, '404 Not Found')
+
+  if env['REQUEST_METHOD'] == 'GET':
+    # TODO: in chunks, don't waste RAM
+    return resp(sr, '200 OK', body=fc.get(key).read())
 
   if env['REQUEST_METHOD'] == 'DELETE':
-    fc.delete(hkey)
+    fc.delete(key)
+    return resp(sr, '200 OK')
 
