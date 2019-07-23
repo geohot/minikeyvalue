2bce892bd657e6f42a8578928d4badbe6b8e53ba
George Hotz <geohot@gmail.com>
2019-07-23 15:41:45 -0400

    add a simple test for the hash function stability

---
+++ lib_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "testing"
+  //"fmt"
+)
+
+// ensure the path hashing function doesn't change
+func Test_key2path(t *testing.T) {
+  tests := map[string]string{
+    "hello": "/5d/41/aGVsbG8=",
+    "helloworld": "/fc/5e/aGVsbG93b3JsZA==",
+  }
+  for k,v := range tests {
+    ret := key2path([]byte(k))
+    if ret != v {
+      t.Fatal("key2path function broke", k, ret, v)
+    }
+  }
+}
+
+// ensure the volume hashing function doesn't change
+func Test_key2volume(t *testing.T) {
+  volumes := []string{"larry", "moe", "curly"}
+  tests := map[string]string{
+    "hello": "larry",
+    "helloworld": "curly",
+    "world": "moe",
+    "blah": "curly",
+  }
+  for k,v := range tests {
+    ret := key2volume([]byte(k), volumes)
+    if ret != v {
+      t.Fatal("key2volume function broke", k, ret, v)
+    }
+  }
+}
+
