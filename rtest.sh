6a5428d4033467e3effd43bb90558b9465eb09f0
George Hotz <geohot@gmail.com>
2019-07-23 19:27:29 -0400

    make rebuild a proper test

--- rtest.sh
+++ rtest.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 ALTDB=/tmp/indexdbalt/
 echo "rebuild and rebalance test"
-echo "Although this works with the master server running, you shouldn't do that!"
-go run rebuild.go lib.go localhost:3001,localhost:3002 $ALTDB
+
+# take down main server
+kill $(pgrep -f "./master")
+
+# rebuild and compare the database
+go run rebuild.go lib.go localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 $ALTDB
+go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
+
+# do a few rebalances
 go run rebalance.go lib.go localhost:3001 $ALTDB
 go run rebalance.go lib.go localhost:3002 $ALTDB
 go run rebalance.go lib.go localhost:3001,localhost:3002 $ALTDB
---
+++ tools/leveldb_compare.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "github.com/syndtr/goleveldb/leveldb"
+  //"github.com/syndtr/goleveldb/leveldb/util"
+)
+
+func main() {
+  db1, err1 := leveldb.OpenFile(os.Args[1], nil)
+  if err1 != nil { panic("db1 open failed") }
+  db2, err2 := leveldb.OpenFile(os.Args[2], nil)
+  if err2 != nil { panic("db2 open failed") }
+
+  iter1 := db1.NewIterator(nil, nil)
+  iter2 := db2.NewIterator(nil, nil)
+  bad := false
+  for iter1.Next() {
+    iter2.Next()
+    k1 := string(iter1.Key())
+    v1 := string(iter1.Value())
+    k2 := string(iter2.Key())
+    v2 := string(iter2.Value())
+    if k1 != k2 {
+      panic(fmt.Sprintf("key mismatch %s != %s", k1, k2))
+    }
+    if v1 != v2 {
+      // we can continue with a value mismatch
+      fmt.Println(k1, v1, k2, v2)
+      bad = true
+    }
+  }
+  if bad { panic("not all values matched") }
+}
+
