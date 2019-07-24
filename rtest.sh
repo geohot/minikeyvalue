84bebe21d636310c663cf44999b7d59d395c099f
George Hotz <geohot@gmail.com>
2019-07-24 10:21:19 -0400

    is that really what it takes for the dockerfile

--- rtest.sh
+++ rtest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ALTDB=/tmp/indexdbalt/
+ALTDB2=/tmp/indexdbalt2/
 echo "rebuild and rebalance test"
 
 # take down main server (now leaves nginx running)
@@ -10,8 +11,12 @@ set -e
 go run rebuild.go lib.go localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 $ALTDB
 go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
 
-# do a rebalances, then put it back
+# do a rebalance, then put it back
 #go run rebalance.go lib.go localhost:3001,localhost:3002,localhost:3003 $ALTDB
 #go run rebalance.go lib.go localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 $ALTDB
 #go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
+
+# rebuild and compare the database
+#go run rebuild.go lib.go localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 $ALTDB2
+#go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB2
 
