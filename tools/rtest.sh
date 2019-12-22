eedf0e293d94ab959fe16feb31a03a434f83bb9b
abriosi <goncalo.abreu@gmail.com>
2019-12-22 13:15:56 +0000

    rtest.sh simplification

--- tools/rtest.sh
+++ tools/rtest.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 ALTDB=/tmp/indexdbalt/
 ALTDB2=/tmp/indexdbalt2/
+if [ "$USERPASS" = "" ]
+then
+   USERPASS=""
+else
+   USERPASS="-userpass $USERPASS"
+fi
 echo "rebuild and rebalance test"
 
 # take down main server (now leaves nginx running)
 kill $(pgrep -f "indexdb")
 set -e
 
-if [[ -z "${USERPASS}" ]]; then
-  # rebuild and compare the database
-  ./mkv -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB rebuild
-  go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
+# rebuild and compare the database
+./mkv $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB rebuild
+go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
 
-  # do a rebalance, then put it back
-  ./mkv -volumes localhost:3001,localhost:3002,localhost:3003 -db $ALTDB rebalance
-  ./mkv -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB rebalance
-  go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
+# do a rebalance, then put it back
+./mkv $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003 -db $ALTDB rebalance
+./mkv $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB rebalance
+go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
 
-  # rebuild and compare the database
-  ./mkv -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB2 rebuild
-  go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB2
-else
-  # rebuild and compare the database
-  ./mkv -userpass $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB rebuild
-  go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
-
-  # do a rebalance, then put it back
-  ./mkv -userpass $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003 -db $ALTDB rebalance
-  ./mkv -userpass $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB rebalance
-  go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB
-
-  # rebuild and compare the database
-  ./mkv -userpass $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB2 rebuild
-  go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB2
-fi
+# rebuild and compare the database
+./mkv $USERPASS -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db $ALTDB2 rebuild
+go run tools/leveldb_compare.go /tmp/indexdb/ $ALTDB2
