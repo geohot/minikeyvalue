b04c99178d147813f8fcb09365ebb9af649589db
abriosi <goncalo.abreu@gmail.com>
2019-12-22 13:27:39 +0000

    bringup refactor

--- tools/bringup.sh
+++ tools/bringup.sh
@@ -2,21 +2,19 @@
 #trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 kill $(pgrep -f nginx)
 
-if [[ -z "${USERPASS}" ]]; then
-  PORT=3001 ./volume /tmp/volume1/ &
-  PORT=3002 ./volume /tmp/volume2/ &
-  PORT=3003 ./volume /tmp/volume3/ &
-  PORT=3004 ./volume /tmp/volume4/ &
-  PORT=3005 ./volume /tmp/volume5/ &
-
-  ./mkv -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db /tmp/indexdb/ server
+if [ "$USERPASS" = "" ]
+then
+   export AUTH=""
+   export AUTHSTR=""
 else
-  echo "basic auth configuration"
-  AUTH=/etc/nginx/.htpasswd PORT=3001 ./volume /tmp/volume1/ &
-  AUTH=/etc/nginx/.htpasswd PORT=3002 ./volume /tmp/volume2/ &
-  AUTH=/etc/nginx/.htpasswd PORT=3003 ./volume /tmp/volume3/ &
-  AUTH=/etc/nginx/.htpasswd PORT=3004 ./volume /tmp/volume4/ &
-  AUTH=/etc/nginx/.htpasswd PORT=3005 ./volume /tmp/volume5/ &
-
-  ./mkv -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -auth /etc/nginx/.htpasswd -db /tmp/indexdb/ server
+   export AUTH=/etc/nginx/.htpasswd
+   export AUTHSTR="-auth /etc/nginx/.htpasswd"
 fi
+
+PORT=3001 ./volume /tmp/volume1/ &
+PORT=3002 ./volume /tmp/volume2/ &
+PORT=3003 ./volume /tmp/volume3/ &
+PORT=3004 ./volume /tmp/volume4/ &
+PORT=3005 ./volume /tmp/volume5/ &
+
+./mkv $AUTHSTR -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db /tmp/indexdb/ server
