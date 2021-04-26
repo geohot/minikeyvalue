8cad2dbaed3b2e02562381780d65606b9c33c010
George Hotz <geohot@gmail.com>
2021-04-25 17:52:17 -0700

    fix permission issue

--- tools/bringup.sh
+++ tools/bringup.sh
@@ -8,5 +8,5 @@ PORT=3003 ./volume /tmp/volume3/ &
 PORT=3004 ./volume /tmp/volume4/ &
 PORT=3005 ./volume /tmp/volume5/ &
 
-./mkv -port 3000 -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db /tmp/indexdb/ server &
+./mkv -port 3000 -volumes localhost:3001,localhost:3002,localhost:3003,localhost:3004,localhost:3005 -db /tmp/indexdb/ server
 
