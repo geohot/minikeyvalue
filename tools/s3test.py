68088b6ede4765353eb251d6cb143cc7b260c69e
George Hotz <geohot@gmail.com>
2021-11-21 09:32:54 -0800

    add expected failure tests until multipart uploads + listing work

--- tools/s3test.py
+++ tools/s3test.py
@@ -11,14 +11,29 @@ class TestS3PyArrow(unittest.TestCase):
   def get_fresh_key(self):
     return "bucket/swag-" + binascii.hexlify(os.urandom(10)).decode('utf-8')
 
-  def test_smallrw(self):
+  @unittest.expectedFailure
+  def test_deletedir(self):
+    s3.delete_dir('bucket')
+
+  # this needs multipart uploads to work
+  @unittest.expectedFailure
+  def test_largerw(self):
+    tbl = pa.table([pa.array(range(10000000))], ['a'])
+
     key = self.get_fresh_key()
-
-    a = pa.array([0,1,2,3])
-    tbl = pa.table([a], ['a'])
     pq.write_table(tbl, key, filesystem=s3)
-
     tbl2 = pq.read_table(key, filesystem=s3)
+    self.assertEqual(tbl, tbl2) # unclear what sort of equality this checks
+    s3.delete_file(key)
+
+  def test_smallrw(self):
+    tbl = pa.table([pa.array([0,1,2,3])], ['a'])
+
+    key = self.get_fresh_key()
+    pq.write_table(tbl, key, filesystem=s3)
+    tbl2 = pq.read_table(key, filesystem=s3)
+    self.assertEqual(tbl, tbl2) # unclear what sort of equality this checks
+    s3.delete_file(key)
 
 if __name__ == '__main__':
   unittest.main()
