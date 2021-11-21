26a83f4977ee2dda0fa3a0038a3a904e60871d94
George Hotz <geohot@gmail.com>
2021-11-21 09:00:16 -0800

    logging and todo

--- src/server.go
+++ src/server.go
@@ -81,7 +81,7 @@ func (a *App) QueryHandler(key []byte, w http.ResponseWriter, r *http.Request) {
 func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	key := []byte(r.URL.Path)
 
-	log.Println(r.Method, r.URL, r.URL.RawQuery)
+	log.Println(r.Method, r.URL, r.ContentLength, r.Header["Range"])
 
 	// this is a list query
 	if len(r.URL.RawQuery) > 0 && r.Method == "GET" {
@@ -148,6 +148,7 @@ func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(302)
 	case "POST":
 		// this will init multipart uploads in "S3"
+		// TODO: add real support for multipart uploads
 	case "PUT":
 		// no empty values
 		if r.ContentLength == 0 {
@@ -188,7 +189,6 @@ func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(500)
 				return
 			}
-			log.Println("wrote", bodylen)
 		}
 
 		var hash = ""
